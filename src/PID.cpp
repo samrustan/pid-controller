9c941c4ff59097eb7bc322a7b33c91fad28a3ad2
Samuel Rustan <samrustan@gmail.com>
2017-08-20 23:40:42 -0500

    basic pid init

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,9 +11,19 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+
+  p_error = i_error = d_error = 0.0;
+  PID::Kp = Kp;
+  PID::Ki = Ki;
+  PID::Kd = Kd;
+
+
 }
 
 void PID::UpdateError(double cte) {
+  p_error = cte;
+  d_error = cte - p_error;
+  i_error = cte + i_error;
 }
 
 double PID::TotalError() {
--- src/main.cpp
+++ src/main.cpp
@@ -32,8 +32,10 @@ int main()
 {
   uWS::Hub h;
 
-  PID pid;
   // TODO: Initialize the pid variable.
+  PID pid;
+
+  pid.Init(0.03, 0.5, 0.1);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,7 +59,14 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
+          //steer_value = 0.0;
+          steer_value = - pid.Kp * pid.p_error
+                        - pid.Kd * pid.d_error
+                        - pid.Ki * pid.i_error;
+
+          // update error
+          pid.UpdateError(fabs(cte));
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
